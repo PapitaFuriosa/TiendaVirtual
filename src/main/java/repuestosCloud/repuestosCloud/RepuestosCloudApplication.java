76663efff61dadab5789177560d7db381fa0e61e
PapitaFuriosa <rojaslencho630@gmail.com>
2026-02-28 20:11:02 -0600

    main

--- src/main/java/repuestosCloud/repuestosCloud/RepuestosCloudApplication.java
+++ src/main/java/repuestosCloud/repuestosCloud/RepuestosCloudApplication.java
@@ -3,9 +3,9 @@ package repuestosCloud.repuestosCloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "repuestosCloud.repuestosCloud")
 public class RepuestosCloudApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(RepuestosCloudApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(RepuestosCloudApplication.class, args);
+    }
 }
